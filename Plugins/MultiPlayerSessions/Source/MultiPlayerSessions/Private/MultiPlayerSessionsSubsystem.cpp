9609476197661078a34d4b0f25570ebdc22345bb
Jason Ye <yzm0614@gmail.com>
2024-11-24 00:27:53 -0500

    Multi session class first initialize

--- Plugins/MultiPlayerSessions/Source/MultiPlayerSessions/Private/MultiPlayerSessionsSubsystem.cpp
+++ Plugins/MultiPlayerSessions/Source/MultiPlayerSessions/Private/MultiPlayerSessionsSubsystem.cpp
@@ -4,11 +4,56 @@
 #include "MultiPlayerSessionsSubsystem.h"
 #include "OnlineSubsystem.h"
 
-UMultiPlayerSessionsSubsystem::UMultiPlayerSessionsSubsystem()
+UMultiPlayerSessionsSubsystem::UMultiPlayerSessionsSubsystem():
+	CreateSessionCompleteDelegate(FOnCreateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionComplete)),
+	FindSessionsCompleteDelegate(FOnFindSessionsCompleteDelegate::CreateUObject(this, &ThisClass::OnFindSessionsComplete)),
+	JoinSessionCompleteDelegate(FOnJoinSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnJoinSessionComplete)),
+	StartSessionCompleteDelegate(FOnStartSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnStartSessionComplete)),
+	DestroySessionCompleteDelegate(FOnDestroySessionCompleteDelegate::CreateUObject(this, &ThisClass::OnDestroySessionComplete))
 {
 	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
 	if (Subsystem)
 	{
 		SessionInterface = Subsystem->GetSessionInterface();
 	}
+}
+
+void UMultiPlayerSessionsSubsystem::CreateSession(int32 NumPublicConnections, FString MatchType)
+{
+}
+
+void UMultiPlayerSessionsSubsystem::FindSessions(int32 MaxSearchResult)
+{
+}
+
+void UMultiPlayerSessionsSubsystem::JoinSession(const FOnlineSessionSearch& SearchResult)
+{
+}
+
+void UMultiPlayerSessionsSubsystem::StartSession()
+{
+}
+
+void UMultiPlayerSessionsSubsystem::DestroySession()
+{
+}
+
+void UMultiPlayerSessionsSubsystem::OnCreateSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiPlayerSessionsSubsystem::OnFindSessionsComplete(bool bWasSuccessful)
+{
+}
+
+void UMultiPlayerSessionsSubsystem::OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result)
+{
+}
+
+void UMultiPlayerSessionsSubsystem::OnStartSessionComplete(FName SessionName, bool bWasSuccessful)
+{
+}
+
+void UMultiPlayerSessionsSubsystem::OnDestroySessionComplete(FName SessionName, bool bWasSuccessful)
+{
 }
--- Plugins/MultiPlayerSessions/Source/MultiPlayerSessions/Public/MultiPlayerSessionsSubsystem.h
+++ Plugins/MultiPlayerSessions/Source/MultiPlayerSessions/Public/MultiPlayerSessionsSubsystem.h
@@ -18,8 +18,34 @@ class MULTIPLAYERSESSIONS_API UMultiPlayerSessionsSubsystem : public UGameInstan
 public:
 	UMultiPlayerSessionsSubsystem();
 
+	void CreateSession(int32 NumPublicConnections, FString MatchType);
+	void FindSessions(int32 MaxSearchResult);
+	void JoinSession(const FOnlineSessionSearch& SearchResult);
+	void StartSession();
+	void DestroySession();
+
 protected:
+	//
+	// Internal Callbacks for delegates
+	//
+	void OnCreateSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnFindSessionsComplete(bool bWasSuccessful);
+	void OnJoinSessionComplete(FName SessionName, EOnJoinSessionCompleteResult::Type Result);
+	void OnStartSessionComplete(FName SessionName, bool bWasSuccessful);
+	void OnDestroySessionComplete(FName SessionName, bool bWasSuccessful);
 
 private:
 	IOnlineSessionPtr SessionInterface;
+
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FOnFindSessionsCompleteDelegate FindSessionsCompleteDelegate;
+	FOnJoinSessionCompleteDelegate JoinSessionCompleteDelegate;
+	FOnStartSessionCompleteDelegate StartSessionCompleteDelegate;
+	FOnDestroySessionCompleteDelegate DestroySessionCompleteDelegate;
+
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
+	FDelegateHandle FindSessionsCompleteDelegateHandle;
+	FDelegateHandle JoinSessionCompleteDelegateHandle;
+	FDelegateHandle StartSessionCompleteDelegateHandle;
+	FDelegateHandle DestroySessionCompleteDelegateHandle;
 };
